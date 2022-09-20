8863d883109b179fa7550eb4772ced91299a4265
Anastasiya Andrushchak <anastasiyaandrushchak@Anastasiyas-MacBook-Pro.local>
2022-09-20 09:29:47 -0400

    Update test

--- test/cdkpipelines-demo.ts
+++ test/cdkpipelines-demo.ts
@@ -4,14 +4,15 @@
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/js-bootcamp-cdk-pipelines-intro-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new JsBootcampCdkPipelinesIntro.JsBootcampCdkPipelinesIntroStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test("SQS Queue Created", () => {
+  console.log("YES! SQS Queue Created");
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new JsBootcampCdkPipelinesIntro.JsBootcampCdkPipelinesIntroStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
 });
